369cb5cb681e9dd7e21f9d6df048d4d1b25624a0
Vikas Thulasi <vikasvicky372@gmail.com>
2024-11-12 09:51:56 +0530

    episode 1 global keyword

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var name = "Vikas"
+var name = "Vikas";
 
-var  a = 20;
+var a = 20;
 var b = 10;
 
 console.log(name);
-console.log(a+b);
+console.log(a + b);
+
+//like window object in browser
+console.log(global);
+
+//this is not pointed to global unlike in browser case
+// it will be empty object {}
+console.log(this);
+
+//in 2020 openJs foundation came up with the keyword, 
+//it'll be referred as global across all platofrms
+
+console.log(globalThis);
+
+
+//it'll be same as global
+console.log(globalThis === global)
