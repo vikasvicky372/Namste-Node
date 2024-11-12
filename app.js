d751b60625bd65b09765ee9101afa3458b5a4e0c
Vikas Thulasi <vikasvicky372@gmail.com>
2024-11-12 11:18:18 +0530

    episode 2 - importing functions and variables from one module to other

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
+const {x,calculate} = require("./sum")
 var name = "Vikas";
 
 var a = 20;
 var b = 10;
 
-console.log(name);
-console.log(a + b);
+console.log(calculate(a,b));
+
+//console.log(name);
+//console.log(a + b);
 
 //like window object in browser
-console.log(global);
+//console.log(global);
 
 //this is not pointed to global unlike in browser case
 // it will be empty object {}
-console.log(this);
+//console.log(this);
 
 //in 2020 openJs foundation came up with the keyword, 
 //it'll be referred as global across all platofrms
 
-console.log(globalThis);
+//console.log(globalThis);
 
 
 //it'll be same as global
---
+++ sum.js
@@ -0,0 +1,7 @@
+var x = "Vikas Thulasi"
+
+function calculate (a,b) {
+    return a+b;
+}
+
+module.exports = {x,calculate};
